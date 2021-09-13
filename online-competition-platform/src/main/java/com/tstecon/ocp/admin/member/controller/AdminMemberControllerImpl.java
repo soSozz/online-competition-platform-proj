8bcf04e4f14c8ac865d6a748319ecca36b2d1cd6
ITHwang <intaek428@gmail.com>
2021-09-13 18:45:13 +0900

    메인페이지 ui 완성

--- online-competition-platform/src/main/java/com/tstecon/ocp/admin/member/controller/AdminMemberControllerImpl.java
+++ online-competition-platform/src/main/java/com/tstecon/ocp/admin/member/controller/AdminMemberControllerImpl.java
@@ -4,27 +4,22 @@ import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import com.tstecon.ocp.member.vo.MemberVO;
 import com.tstecon.ocp.admin.member.service.AdminMemberService;
 import com.tstecon.ocp.common.base.BaseController;
+import com.tstecon.ocp.member.vo.MemberVO;
 
-@Controller("memberController")
+@Controller("adminMemberController")
 public class AdminMemberControllerImpl extends BaseController implements AdminMemberController {
 	@Autowired
 	private AdminMemberService adminMemberService;
-	
+
 	@Override
 	@RequestMapping(value = "/admin/member/listMembers.do", method = RequestMethod.GET)
 	public ModelAndView listMembers(HttpServletRequest request, HttpServletResponse response) throws Exception {
@@ -36,16 +31,16 @@ public class AdminMemberControllerImpl extends BaseController implements AdminMe
 		return mav;
 	}
 
-	@RequestMapping(value = "/member/*Form.do", method = RequestMethod.GET)
-	private ModelAndView form(@RequestParam(value = "result", required = false) String result,
-			HttpServletRequest request, HttpServletResponse response) throws Exception {
-		// String viewName = getViewName(request);
-		String viewName = (String) request.getAttribute("viewName");
-		ModelAndView mav = new ModelAndView();
-		mav.addObject("result", result);
-		mav.setViewName(viewName);
-		return mav;
-	}
+//	@RequestMapping(value = "/member/*Form.do", method = RequestMethod.GET)
+//	private ModelAndView form(@RequestParam(value = "result", required = false) String result,
+//			HttpServletRequest request, HttpServletResponse response) throws Exception {
+//		// String viewName = getViewName(request);
+//		String viewName = (String) request.getAttribute("viewName");
+//		ModelAndView mav = new ModelAndView();
+//		mav.addObject("result", result);
+//		mav.setViewName(viewName);
+//		return mav;
+//	}
 
 	private String getViewName(HttpServletRequest request) throws Exception {
 		String contextPath = request.getContextPath();
---
+++ online-competition-platform/src/main/webapp/WEB-INF/views/common/baseLayout.jsp
@@ -0,0 +1,30 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+	<div id="outer_wrap">
+		<div id="wrap">
+			<header>
+				   <tiles:insertAttribute name="header" />
+			</header>
+			<div class="clear"></div>
+			<aside>
+				 <tiles:insertAttribute name="side" />
+			</aside>
+			<article>
+			 	<tiles:insertAttribute name="body" />
+			</article>
+			<div class="clear"></div>
+			<footer>
+        		<tiles:insertAttribute name="footer" />
+        	</footer>
+		</div>
+		 <tiles:insertAttribute name="quickMenu" />
+    </div>        	
+</body>      
+</html>
