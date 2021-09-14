e0cc41c3b89fcfbc359948d44772d5d1f2324261
ji0e <ljk1448@naver.com>
2021-09-14 15:41:44 +0900

    jsp 구현

--- online-competition-platform/src/main/java/com/tstecon/ocp/admin/stat/dao/AdminStatDAOImpl.java
+++ online-competition-platform/src/main/java/com/tstecon/ocp/admin/stat/dao/AdminStatDAOImpl.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
 import org.springframework.stereotype.Repository;
 
+import com.tstecon.ocp.admin.stat.vo.AdminStatVO;
 import com.tstecon.ocp.notice.compet.vo.NoticeCompetVO;
 @Repository("adminStatDAO")
 public class AdminStatDAOImpl {
