bcff4704d5e583745412e0fdae287124b929615c
MONTLION <ropun1995@gmail.com>
2021-09-14 10:56:10 +0900

    로그인&로그아웃 구현

---
+++ online-competition-platform/src/main/java/com/tstecon/ocp/member/dao/MemberDAO.java
@@ -0,0 +1,15 @@
+package com.tstecon.ocp.member.dao;
+
+import java.util.Map;
+
+import org.springframework.dao.DataAccessException;
+
+import com.tstecon.ocp.member.vo.MemberVO;
+
+public interface MemberDAO {
+	public MemberVO login(Map loginMap) throws DataAccessException;
+
+	public void insertNewMember(MemberVO memberVO) throws DataAccessException;
+
+	public String selectOverlappedID(String id) throws DataAccessException;
+}
