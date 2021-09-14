5ea1108bdea2a02872896b329091bbaa730ebfda
BIG11 <BIG11@BIG11>
2021-09-14 17:12:32 +0900

    통계 어드민 vo, dao 공지사항 수정

--- online-competition-platform/src/main/java/com/tstecon/ocp/admin/stat/dao/AdminStatDAO.java
+++ online-competition-platform/src/main/java/com/tstecon/ocp/admin/stat/dao/AdminStatDAO.java
@@ -1,5 +1,18 @@
 package com.tstecon.ocp.admin.stat.dao;
 
-public class AdminStatDAO {
+import java.util.List;
 
+import org.springframework.dao.DataAccessException;
+
+import com.tstecon.ocp.admin.stat.vo.AdminStatVO;
+
+
+public interface AdminStatDAO {
+	public List<AdminStatVO> selectAdminStatContentsList() throws DataAccessException;
+	
+	public List<AdminStatVO> selectAdminStatLikesList() throws DataAccessException;
+	
+	public List<AdminStatVO> selectAdminStatCmtList() throws DataAccessException;
+	
+	public List<AdminStatVO> selectAdminStatViewList() throws DataAccessException;
 }
