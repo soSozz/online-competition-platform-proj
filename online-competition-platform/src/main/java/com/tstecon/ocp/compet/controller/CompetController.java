9efc9bdee8ba111079a6f08fe1edc34c3658a806
ITHwang <intaek428@gmail.com>
2021-09-17 10:41:11 +0900

    대회 목록 추가
    # WARNING: head commit changed in the meantime
    
    add targetfile

--- online-competition-platform/src/main/java/com/tstecon/ocp/compet/controller/CompetController.java
+++ online-competition-platform/src/main/java/com/tstecon/ocp/compet/controller/CompetController.java
@@ -12,18 +12,24 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.tstecon.ocp.qna.vo.CompetQnaVO;
 
-
-
 public interface CompetController {
 //	대회안내 페이지
-public ModelAndView CompetInfo(@RequestParam("compet_id") int compet_id,HttpServletRequest request, HttpServletResponse reponse) throws Exception;
+	public ModelAndView CompetInfo(@RequestParam("compet_id") int compet_id, HttpServletRequest request,
+			HttpServletResponse reponse) throws Exception;
+
 //문의 페이지
-public ModelAndView Compet_qna_list(HttpServletRequest request, HttpServletResponse response) throws Exception;
+	public ModelAndView Compet_qna_list(HttpServletRequest request, HttpServletResponse response) throws Exception;
+
 //문의 글쓰기창 로그인, 로그아웃
-public ModelAndView Compet_qna_login(@ModelAttribute("member")CompetQnaVO competqnaVO, RedirectAttributes rAttr,  HttpServletRequest request, HttpServletResponse response) throws Exception;
-public ModelAndView  Compet_qna_logout(HttpServletRequest request, HttpServletResponse response) throws Exception;
+	public ModelAndView Compet_qna_login(@ModelAttribute("member") CompetQnaVO competqnaVO, RedirectAttributes rAttr,
+			HttpServletRequest request, HttpServletResponse response) throws Exception;
+
+	public ModelAndView Compet_qna_logout(HttpServletRequest request, HttpServletResponse response) throws Exception;
+
 //문의 글쓰기 추가
-public ResponseEntity addNewArticle(MultipartHttpServletRequest multipartRequest,HttpServletResponse response) throws Exception;
+	public ResponseEntity addNewArticle(MultipartHttpServletRequest multipartRequest, HttpServletResponse response)
+			throws Exception;
+
+	// 대회목록 페이지
+	public ModelAndView competList(HttpServletRequest request, HttpServletResponse response) throws Exception;
 }
-
-
