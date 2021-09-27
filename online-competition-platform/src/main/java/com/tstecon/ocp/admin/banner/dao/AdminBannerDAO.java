34c203da30d2412ec0748b66d266ecb56426f8d9
BIG19 <BIG19@BIG19>
2021-09-27 12:06:19 +0900

    admin 배너 관리

---
+++ online-competition-platform/src/main/java/com/tstecon/ocp/admin/banner/dao/AdminBannerDAO.java
@@ -0,0 +1,7 @@
+package com.tstecon.ocp.admin.banner.dao;
+
+import java.util.List;
+
+public interface AdminBannerDAO {
+public List	selectBannerName();
+}
---
+++ online-competition-platform/src/main/java/com/tstecon/ocp/admin/banner/service/AdminBannerService.java
@@ -0,0 +1,7 @@
+package com.tstecon.ocp.admin.banner.service;
+
+import java.util.List;
+
+public interface AdminBannerService {
+	  public List selectBannerName();
+}
